970335bbdbe84791689b5c57f144d58a683da88b
Arkapratim Mondal <arkapratim.mondal@student.manchester.ac.uk>
2024-12-19 21:27:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,203 @@
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "./App.css";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+
+function App() {
+
+
+  const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6cba01336dmsh8d4528578b58f1dp177179jsndfeb6f2a850e",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  const WEATHER_API_KEY = "22be1151d380726ffda1c9af8668626a";
+
+
+  const CurrentWeather = ({ data }) => {
+    return (
+      <div className="weather">
+        <div className="top">
+          <div>
+            <p className="city">{data.city}</p>
+            <p className="weather-description">{data.weather[0].description}</p>
+          </div>
+          <img
+            alt="weather"
+            className="weather-icon"
+            src={`icons/${data.weather[0].icon}.png`}
+          />
+        </div>
+        <div className="bottom">
+          <p className="temperature">{Math.round(data.main.temp)}°C</p>
+          <div className="details">
+            <div className="parameter-row">
+              <span className="parameter-label">Details</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Feels like</span>
+              <span className="parameter-value">
+                {Math.round(data.main.feels_like)}°C
+              </span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Wind</span>
+              <span className="parameter-value">{data.wind.speed} m/s</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Humidity</span>
+              <span className="parameter-value">{data.main.humidity}%</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Pressure</span>
+              <span className="parameter-value">{data.main.pressure} hPa</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+
+  const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+    
+    return (
+      <>
+        <label className="title">Daily</label>
+        <Accordion allowZeroExpanded>
+          {data.list.splice(0, 7).map((item, idx) => (
+            <AccordionItem key={idx}>
+              <AccordionItemHeading>
+                <AccordionItemButton>
+                  <div className="daily-item">
+                    <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
+                    <label className="day">{forecastDays[idx]}</label>
+                    <label className="description">{item.weather[0].description}</label>
+                    <label className="min-max">{Math.round(item.main.temp_max)}°C /{Math.round(item.main.temp_min)}°C</label>
+                  </div>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                <div className="daily-details-grid">
+                  <div className="daily-details-grid-item">
+                    <label>Pressure:</label>
+                    <label>{item.main.pressure}</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Humidity:</label>
+                    <label>{item.main.humidity}</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Clouds:</label>
+                    <label>{item.clouds.all}%</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Wind speed:</label>
+                    <label>{item.wind.speed} m/s</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Sea level:</label>
+                    <label>{item.main.sea_level}m</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Feels like:</label>
+                    <label>{item.main.feels_like}°C</label>
+                  </div>
+                </div>
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </>
+    );
+  };
+  
+
+  const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+  
+    const loadOptions = (inputValue) => {
+      return fetch(
+        `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        geoApiOptions
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data.map((city) => {
+              return {
+                value: `${city.latitude} ${city.longitude}`,
+                label: `${city.name}, ${city.countryCode}`,
+              };
+            }),
+          };
+        });
+    };
+  
+    const handleOnChange = (searchData) => {
+      setSearch(searchData);
+      onSearchChange(searchData);
+    };
+  
+    return (
+      <AsyncPaginate
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+      />
+    );
+  };
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+      <h1>Monark Weather Website</h1>
+      </div>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
+
+export default App;
